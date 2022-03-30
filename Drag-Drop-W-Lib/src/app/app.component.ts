b76445113f65c988665459dbd78a8add48b9e18e
Thibeau <thibeau.deleu@student.howest.be>
2022-03-30 14:43:24 +0200

    First version live, stille some issues with placeholder and placement cards after drop

--- Drag-Drop-W-Lib/src/app/app.component.ts
+++ Drag-Drop-W-Lib/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { CdkDragDrop, CdkDragEnter, CdkDragMove, moveItemInArray } from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'Drag-Drop-W-Lib';
+  items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  drop(event: CdkDragDrop<any>) {
+    this.items[event.previousContainer.data.index] = event.container.data.item;
+    this.items[event.container.data.index] = event.previousContainer.data.item;
+    moveItemInArray(this.items, event.container.data.item, event.previousContainer.data.item);
+  } 
+
+    public test(): void {
+      console.log('CLICK')
+    }
 }
--- Drag-Drop-W-Lib/src/app/app.module.ts
+++ Drag-Drop-W-Lib/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
